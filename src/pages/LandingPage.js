f4d50b01260c62a5283c3e068a95e9177c4d52ba
syahrul35 <irul.syahrulmaulana@gmail.com>
2022-10-07 20:54:17 +0700

    frist commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import moduleName from 'elements/Button'
+
+export default class landingPage extends Component {
+  render() {
+    return (
+      <div>landingPage</div>
+    )
+  }
+}
